b9afea5ec6abdca5ca862e871643fa3d2ba0fbc9
777zal <s.rizal91@gmail.com>
2024-09-12 14:54:07 +0700

    add shell scrip for gerber creation automation

---
+++ run_prod.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+kicad-cli pcb export gerbers -o Doc/ \
+                             -l F.Cu,In1.Cu,In2.Cu,B.Cu,F.Paste,B.Paste,F.Silkscreen,B.Silkscreen,F.Mask,B.Mask,Edge.Cuts\
+                             --ev --no-x2 --no-netlist \
+                             --subtract-soldermask \
+                             ./scale.kicad_pcb
+
+kicad-cli pcb export drill -o Doc/ \
+                           --format excellon \
+                           --drill-origin absolute \
+                           --excellon-zeros-format decimal \
+                           --excellon-oval-format route \
+                           --excellon-units in \
+                           --generate-map \
+                           --excellon-separate-th \
+                           --map-format gerberx2 \
+                           ./scale.kicad_pcb
