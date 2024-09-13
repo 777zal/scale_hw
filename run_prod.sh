78e97692df2c311b4fecd9c32ad4ace6e18f9135
777zal <s.rizal91@gmail.com>
2024-09-13 17:02:52 +0700

    adding project name as gerber file name

--- run_prod.sh
+++ run_prod.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
 
-kicad-cli pcb export gerbers -o Doc/ \
+mkdir -p  PROD/GBR
+
+filepath=`pwd`
+echo $filepath
+
+file_path=`find $filepath -name "*.kicad_pro"`
+file_name="${file_path##*/}"
+name="${file_name%.*}"
+echo $name
+
+kicad-cli pcb export gerbers -o PROD/GBR/ \
                              -l F.Cu,In1.Cu,In2.Cu,B.Cu,F.Paste,B.Paste,F.Silkscreen,B.Silkscreen,F.Mask,B.Mask,Edge.Cuts\
                              --ev --no-x2 --no-netlist \
                              --subtract-soldermask \
                              ./scale.kicad_pcb
 
-kicad-cli pcb export drill -o Doc/ \
+kicad-cli pcb export drill -o PROD/GBR/ \
                            --format excellon \
                            --drill-origin absolute \
                            --excellon-zeros-format decimal \
@@ -18,4 +28,4 @@ kicad-cli pcb export drill -o Doc/ \
                            ./scale.kicad_pcb
 
 cd PROD/GBR/
-zip -r ../../PROD/GBR.zip .
+zip -r ../../PROD/$name"_gerber".zip .
